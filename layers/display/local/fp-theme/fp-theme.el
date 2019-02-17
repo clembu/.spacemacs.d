d47a24fa6069e007264ac51fe90984a418bb7a1e
Clément Busschaert <clement.busschaert@gmail.com>
2019-02-17 17:20:58 +0100

    Theme: margins+modeline+fontlocks+company+tuareg+treemacs

--- layers/display/local/fp-theme/fp-theme.el
+++ layers/display/local/fp-theme/fp-theme.el
@@ -93,33 +93,225 @@ influence of C1 on the result."
    'fp
 
    ;; BASICS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-   `(cursor ((,class (:background ,bg-sel :foreground ,fg))))
-   `(default ((,class (:background ,bg-dark :foreground ,fg))))
-   `(hl-line ((,class (:background ,bg))))
-   `(fringe ((,class (:background ,bg-dark2 :foreground ,fg))))
-   `(escape-glyph ((,class (:foreground ,wng :weight bold))))
+   ;{{{
+   `(cursor ;; Default cursor. Will be overridden by mode color
+     ((,class (:background ,bg-sel :foreground ,fg))))
+   `(default ;; Default face. Back ground is the buffer background.
+      ((,class (:background ,bg-dark :foreground ,fg))))
+   `(shadow ;; Less noticeable face
+     ((,class (:foreground ,(funcall fg3 bg-dark)))))
+   `(hl-line ;; Current line when highlighted
+     ((,class (:background ,bg))))
+   `(escape-glyph ;; Anything that corresponds to a control character.
+     ;; Like, `^C' or `^@'
+     ((,class (:foreground ,wng :weight bold))))
+   `(region ;; Selected region.
+     ((,class (:background ,(col/mix bg-sel bg-dark 0.2)))))
+   `(trailing-whitespace ;; Trailing white-space when highlighted
+     ((,class (:background ,wng))))
 
+   `(error ((,class (:foreground ,err :weight bold))))
    `(success ((,class (:foreground ,succ :weight bold))))
    `(warning ((,class (:foreground ,wng :weight bold))))
    `(tooltip ((,class (:foreground ,bg-dark2 :foreground ,fg))))
    `(completions-annotations ((,class (:foreground ,fg :background ,bg-light))))
 
+   `(vertical-border ;; Separator between windows
+     ((,class (:foreground ,bd-dark))))
+   ;}}}
+
+   ;; MARGINS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;{{{
+   `(line-number ((,class (:background ,bg-dark2 :foreground ,(funcall fg3 bg-dark2)))))
+   `(line-number-current-line
+     ((,class (:foreground ,fg))))
+   `(fringe ((,class (:background ,bg-dark2 :foreground ,fg))))
+                                        ; GIT GUTTER ;;;;;;;;;;;;;;;;;;;;;;;;;;
+   `(git-gutter+-added ((,class (:foreground ,succ))))
+   `(git-gutter+-deleted ((,class (:foreground ,destr))))
+   `(git-gutter+-modified ((,class (:foreground ,wng))))
+   `(git-gutter+-separator ((,class (:foreground ,bg-sel))))
+   ;}}}
+
+   ;; MODE-LINE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;{{{
+   `(mode-line-buffer-id ((,class (:foreground ,acc :weight bold))))
+   `(mode-line
+     ((,class (:foreground ,fg
+                           :background ,bg-dark
+                           :box (:color ,bd-light :line-width -1 :style released-button)
+                           ))
+      (t :inverse-video t)))
+   `(powerline-active1
+     ((,class (:foreground ,bg-light
+                           :background ,acc
+                           :box (:color ,bd-light :line-width -1 :style released-button)
+                           ))))
+   `(powerline-active2
+     ((,class (:foreground ,bg-light
+                           :background ,acc
+                           :box (:color ,bd-light :line-width -1 :style released-button)
+                           ))))
+                                        ; INACTIVE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   `(mode-line-inactive
+     ((,class (:foreground ,(funcall fg3 bg-dark)
+                           :background ,bg-dark
+                           :box nil
+                           ))))
+   `(powerline-inactive1
+     ((,class (:foreground ,(funcall fg3 acc)
+                           :background ,(col/mix acc bg-panel 0.02)
+                           :box nil))))
+   `(powerline-inactive2
+     ((,class (:foreground ,(funcall fg3 bg-dark)
+                           :background ,(col/mix acc bg-panel 0.02)
+                           :box nil))))
+   ;}}}
+
    ;; ISEARCH ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;{{{
    `(isearch ((,class (:background ,bg-sel :foreground ,bg-dark))))
    `(isearch-fail ((,class (:background ,bg-error :foreground ,fg))))
    `(lazy-highlight ((,class (:background ,bg-light :foreground ,ph))))
+   ;}}}
 
    ;; GUI ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;{{{
    `(highlight ((,class (:background ,fg :foreground ,bg-light))))
    `(button ((,class (:foreground ,fg :background ,bg2 :underline t))))
    `(link ((,class (:foreground ,ln :underline t :weight bold))))
    `(link-visited ((,class (:foreground ,lnv :underline t :weight normal))))
    `(widget-field ((,class (:foreground ,ph :background ,bg-light))))
 
+   `(header-line ((,class (:foreground ,acc :background ,bg-panel))))
+   ;}}}
+
    ;; FONT LOCKS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;{{{
+                                        ; TEXT ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                        ;{{{
    `(font-lock-comment-face ((,class (,@(w/bg bg-dark2 fg 0.3) :italic t))))
+   `(font-lock-comment-delimiter-face ((,class (,@(w/bg bg-dark2 acc 0.5) :weight bold))))
+   `(font-lock-doc-face ((,class (:foreground ,accl :background ,bg))))
+                                        ;}}}
+
+                                        ; WORDS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                        ;{{{
    `(font-lock-keyword-face ((,class (:foreground ,acc :weight bold :italic t))))
-   `(font-lock-builtin-face ((,class (:foreground ,(funcall acc2 bg-dark)))))))
+   `(font-lock-builtin-face ((,class (:foreground ,(funcall acc2 bg-dark)))))
+   `(font-lock-function-name-face ((,class (:foreground ,yellow :weight bold))))
+   `(font-lock-variable-name-face ((,class (:foreground ,yellow :italic t))))
+                                        ;}}}
+
+                                        ; VALUES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                        ;{{{
+   `(font-lock-constant-face ((,class (:foreground ,green))))
+   `(font-lock-string-face ((,class (:foreground ,green))))
+   `(font-lock-type-face ((,class (:foreground ,blue))))
+   `(font-lock-negation-char-face ((,class (:foreground ,green))))
+                                        ;}}}
+
+                                        ; HELPERS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                        ;{{{
+   `(font-lock-preprocessor-face ((,class (:foreground ,red))))
+   `(font-lock-regexp-grouping-construct ((,class (:foreground ,yellow :weight bold))))
+   `(font-lock-regexp-grouping-backslash ((,class (:foreground ,green :weight bold))))
+   `(font-lock-warning-face ((,class (:foreground ,wng :weight bold))))
+                                        ;}}}
+   ;}}}
+
+   ;; COMPANY ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;{{{
+   `(company-tooltip ; Company's auto-completions list background
+     ((,class (:background ,(col/mix bg-info bg-dark 0.3)
+                           :foreground ,(funcall fg2 bg-info)))))
+   `(company-scrollbar-bg ; Company's auto-completions list scroll-bar background
+     ((,class (:background ,(col/mix bg-info bg-dark 0.2)))))
+   `(company-scrollbar-fg ; Company's auto-completions list scroll-bar foreground
+     ((,class (:background ,(col/mix bg-info bg-dark 0.4)))))
+   `(company-tooltip-selection ; Company's auto-completion selected item
+     ((,class (:background ,bg-info :foreground ,bg-panel))))
+   `(company-tooltip-common ; Company's auto-completions base-match
+     ((,class (:foreground ,yellow))))
+   `(company-tooltip-common-selection ; Company's auto-completions base-match on selection
+     ((,class (:foreground ,red))))
+   `(company-tooltip-annotation ; Company's auto-completions comments (e.g. snippet name)
+     ((,class (:foreground ,wng))))
+   `(company-tooltip-annotation-selection ; Company's auto-completions comments on selection
+     ((,class (:foreground ,bg-light))))
+   `(company-preview ; Company's auto-completion preview
+     ((,class (:background ,acc :foreground ,bg-panel))))
+   `(company-preview-common ; Company's auto-completion preview base-match
+     ((,class (:foreground ,bg-panel))))
+                                        ;}}}
+
+   ;; TUAREG ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;{{{
+                                        ; WORDS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                        ;{{{
+   `(tuareg-font-lock-governing-face ; Ocaml code-flow keywords
+     ((,class (:foreground ,accl :weight bold))))
+   `(tuareg-font-lock-constructor-face ; Ocaml type constructor
+     ((,class (:foreground ,(col/mix blue fg)))))
+   `(tuareg-font-lock-module-face ; Ocaml module
+     ((,class (:foreground ,blue :weight bold :italic t))))
+   `(tuareg-font-lock-label-face ; Ocaml named arguments
+     ((,class (:foreground ,accl))))
+                                        ;}}}
+
+                                        ; SYMBOLS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                        ;{{{
+   `(tuareg-font-double-colon-face ; Ocaml sequence terminator
+     ((,class (:foreground ,(funcall fg2 bg-dark) :weight bold))))
+   `(tuareg-font-lock-operator-face ; Ocaml operators
+     ((,class (:foreground ,yellow))))
+                                        ;}}}
+
+                                        ; ERRORS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                        ;{{{
+   `(tuareg-font-lock-error-face ; Ocaml errors
+     ((,class (:foreground ,fg-sel :weight bold :background ,bg-error))))
+   `(tuareg-font-lock-interactive-error-face
+     ((,class (:foreground ,err :weight bold))))
+                                        ;}}}
+
+                                        ; ANNOTATIONS ;;;;;;;;;;;;;;;;;;;;;;;;;
+                                        ;{{{
+   `(tuareg-font-lock-attribute-face ; Ocaml attribute annotations ( [@@attributes])
+     ((,class (:foreground ,wng))))
+   `(tuareg-font-lock-extension-node-face ; Ocaml extensions ([%%extension])
+     ((,class (:foreground ,wng :weight bold))))
+   `(tuareg-font-lock-infix-extension-node-face ; Ocaml extensions (let% ?)
+     ((,class (:foreground ,wng))))
+                                        ;}}}
+   ;}}}
+
+   ;; TREEMACS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;{{{
+   `(treemacs-root-face
+     ((,class (:foreground ,blue :height 140 :weight bold))))
+   `(treemacs-directory-face
+     ((,class (:foreground ,accl))))
+   `(treemacs-term-node-face
+     ((,class (:foreground ,green))))
+                                        ; GIT STATES ;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                        ;{{{
+   `(treemacs-git-added-face
+     ((,class (:foreground ,succ))))
+   `(treemacs-git-conflict-face
+     ((,class (:foreground ,err :weight bold))))
+   `(treemacs-git-ignored-face
+     ((,class (:foreground ,(funcall fg3 bg-dark)))))
+   `(treemacs-git-modified-face
+     ((,class (:foreground ,wng))))
+   `(treemacs-git-renamed-face
+     ((,class (:foreground ,wng :italic t :weight bold))))
+   `(treemacs-git-untracked-face
+     ((,class (:foreground ,(funcall acc2 bg-dark)))))
+                                        ;}}}
+
+   ;}}}
+   ))
 
 ;;;###autoload
 (and load-file-name
