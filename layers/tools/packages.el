1925f9146cef0c45971305624c999e99975bb8ba
Clement Busschaert <clement.busschaert@gmail.com>
2019-03-21 17:07:41 +0100

    [tools] keychain environment for ssh

--- layers/tools/packages.el
+++ layers/tools/packages.el
@@ -2,9 +2,14 @@
       '(; UNOWNED ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
         ranger
                                         ; OWNED ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        keychain-environment
                                         ; LOCAL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
         ))
 
 (defun tools/pre-init-ranger ()
   (setq ranger-cleanup-on-disable t)
   (setq ranger-enter-with-minus t))
+
+(defun tools/init-keychain-environment ()
+  (use-package keychain-environment
+    :init (call-interactively #'keychain-refresh-environment)))
